45b17e794f7c87c7dfe4e16368d2de4ca361da04
Henrique <carlospaisca@gmail.com>
2017-06-28 23:20:56 -0300

    ajustes no parser preparação para craicao da AST

--- parser.py
+++ parser.py
@@ -6,221 +6,219 @@ from lexer import tokens
 
 print tokens
 
-def p_program:
-    """ <program> => <SequenciaDeclaracao> """
+def p_program():
+    ''' program : SequenciaDeclaracao'''
+    p[0] : p[1]
     pass
 
 def p_declaracao():
-    """ 
-    <declaracao> => <declaracaoVariavel>
-                  | id '(' <listaParametros> ')' '{' <bloco> '}'
-                  | <tipo> id '(' <listaParametros> ')' '{' <bloco> '}'
-    """
+
+    '''
+    declaracao : declaracaoVariavel
+                  | id ABREPAREN listaParametros FECHAPAREN ABRECHAVE bloco FECHACHAVE
+                  | tipo id ABREPAREN listaParametros FECHAPAREN ABRECHAVE bloco FECHACHAVE
+    '''
+
     pass
 
 def p_declaracao_variavel():
-    """ <declaracaoVariavel> => <tipo> <sequenciaSpecVariavel> ';' """
+    '''declaracaoVariavel : tipo sequenciaSpecVariavel PONTOVIRGULA'''
     pass
 
-def p_especificacao_variavel():
-    """
-    <especificacaoVariavel> => id
-                             | id '=' <literal>
-                             | id '[' numero ']'
-                             | id '[' numero ']' '=' '{' <sequenciaLiteral> '}'
-    """
-    pass
+    p[0] : Node ('declaracaoVariavel', [[p1], p[2]])
+
+def p_especificacao_variavel(p):
+    '''
+    especificacaoVariavel : ID
+                             | ID ATRIB literal
+                             | ID ABRECOLCH numero FECHACOLCH
+                             | ID ABRECOLCH numero FECHACOLCH ATRIB ABRECHAVE sequenciaLiteral FECHACHAVE
+    '''
+    if len(p) :: 2:
+        p[0] : Node('especificacaoVariavelID', [], p[1])
+    elif len p[1]
 
 def p_tipo():
-    """
-    <tipo> => "int"
-            | "string"
-            | "bool"
-    """
+    '''
+    tipo : INT
+         | STRING
+         | BOOL
+    '''
     pass
 
 def p_parametros():
-    """
-    <parametros> => <tipo> id
-                  | <tipo> id '[' ']'
-    """
+    '''
+    parametros : tipo ID
+               | tipo ID ABRECOLCH FECHACOLCH
+    '''
     pass
 
 def p_bloco():
-    """ <bloco> => <listaDeclaracaoVariavel> <listaComandos> """
+    ''' bloco : listaDeclaracaoVariavel listaComandos'''
     pass
 
 def p_comando():
-    """
-    <comando> => <comandoIF>
-               | <comandoWhile>
-               | <comandoFor>
-               | <comandoBreak>
-               | <comandoReturn>
-               | <comandoRead>
-               | <comandoWrite>
-               | <atribuicao> ';'
-               | <chamada> ';'
-    """
+    '''
+      comando : comandoIF
+              | comandoWhile
+              | comandoFor
+              | comandoBreak
+              | comandoReturn
+              | comandoRead
+              | comandoWrite
+              | atribuicao PONTOVIRGULA
+              | chamada PONTOVIRGULA
+    '''
     pass
 
 def p_comando_if():
-    """
-    <comandoIf> => "if" '(' <expressao> ')' '{' <bloco> '}'
-                 | "if" '(' <expressao> ')' '{' <bloco> '}' "else" '{' <bloco> '}'
-    """
+    '''
+    comandoIf : IF ABREPAREN expressao FECHAPAREN ABRECOLCH bloco FECHACOLCH
+                | IF ABREPAREN expressao FECHAPAREN FECHACOLCH bloco FECHACOLCH ELSE ABRECOLCH bloco FECHACOLCH
+    '''
     pass
 
 def p_comando_while():
-    """ <comandoWhile> => "while" '(' <expressao> ')' '{' <bloco> '}' """
+    ''' comandoWhile : WHILE ABREPAREN expressao FECHAPAREN ABRECHAVE bloco FECHACHAVE '''
     pass
 
 def p_comando_for():
-    """ <comandoFor> => "for" '(' <atribuicao> ';' <expressao> ';' <atribuicao> ')' '{' <bloco> '}' """
+    '''comandoFor : FOR ABREPAREN atribuicao PONTOVIRGULA expressao PONTOVIRGULA atribuicao FECHAPAREN ABRECOLCH bloco FECHACOLCH'''
     pass
 
 def p_comando_break():
-    """ <comandoBreak> => "break" ';' """
+    '''comandoBreak : BREAK PONTOVIRGULA'''
     pass
 
 def p_comando_read():
-    """ <comandoRead> => "read" <variavel> ';' """
+    '''comandoRead : read variavel PONTOVIRGULA'''
     pass
 
 def p_comando_write():
-    """ <comandoWrite> => "write" <listaExpressao> ';' """
+    '''comandoWrite : write listaExpressao PONTOVIRGULA'''
     pass
 
 def p_comando_return():
-    """ 
-    <comandoReturn> => "return" ';'
-                     | "return" <expressao> ';'
-    """
+    '''
+    comandoReturn : RETURN PONTOVIRGULA
+                  |RETURN expressao PONTOVIRGULA
+    '''
     pass
 
 def p_chamada():
-    """ <chamada> => id '(' <listaExpressao> ')' """
+    '''chamada : ID ABREPAREN listaExpressao FECHAPAREN'''
     pass
 
 def p_atribuicao():
-    """
-    <atribuicao> => <variavel> '='  <expressao>
-                  | <variavel> "+=" <expressao>
-                  | <variavel> "-=" <expressao>
-                  | <variavel> "*=" <expressao>
-                  | <variavel> "/=" <expressao>
-                  | <variavel> "%=" <expressao> 
-    """
+'''
+    atribuicao : variavel ATRIB  expressao
+              | variavel MAISATRIB expressao
+              | variavel MENOSATRIB expressao
+              | variavel MULTATRIB expressao
+              | variavel DIVATRIB expressao
+              | variavel MODATRIB expressao
+    '''
     pass
 
 def p_variavel():
-    """
-    <variavel> => id
-                | id '[' <expressao> ']'
-    """    
+    '''
+    variavel : ID
+             | ID ABRECOLCH expressao FECHACOLCH
+    '''
     pass
 
 def p_expressao():
-    """
-    <expressao> => <expressao> '+'   <expressao>
-                 | <expressao> '-'   <expressao>
-                 | <expressao> '*'   <expressao>
-                 | <expressao> '/'   <expressao>
-                 | <expressao> '%'   <expressao>
-                 | <expressao> "=="  <expressao>
-                 | <expressao> "!="  <expressao>
-                 | <expressao> "<="  <expressao>
-                 | <expressao> ">="  <expressao>
-                 | <expressao> '>'   <expressao>
-                 | <expressao> '<'   <expressao>
-                 | <expressao> "&&"  <expressao>
-                 | <expressao> "||"  <expressao>
-                 | '!' <expressao>
-                 | '-' <expressao>
-                 | <expressao> '?' <expressao> ':' <expressao>
-                 | <chamada>
-                 | <variavel>
-                 | <literal>
-                 | '(' <expressao> ')'
-    """
+    '''
+    expressao : expressao MAIS  expressao
+                 | expressao MENOS   expressao
+                 | expressao MULT   expressao
+                 | expressao DIV   expressao
+                #  | expressao '%'   expressao
+                 | expressao IGUAL  expressao
+                 | expressao DIFERENTE  expressao
+                 | expressao MENORIGUAL  expressao
+                 | expressao MAIORIGUAL  expressao
+                 | expressao MAIOR   expressao
+                 | expressao MENOR   expressao
+                 | expressao E  expressao
+                 | expressao OU  expressao
+                 | NEG expressao
+                #  | '-' expressao
+                 | expressao TERNARIOSE expressao TERNARIOSENAO expressao
+                 | chamada
+                 | variavel
+                 | literal
+                 | ABREPAREN expressao FECHAPAREN
+    '''
     pass
 
 def p_literal():
-    """
-    <literal> => numero
-               | string
-               | logico
-    """
+'''
+    literal : NUMERO
+               | STRING
+               | BOOL
+    '''
     pass
 
 def p_lista_parametros():
-    """
-    <listaParametros> => <sequenciaParametros>
+'''
+    listaParametros : sequenciaParametros
                  | ε
-    """
+    '''
     pass
 
 def p_sequencia_parametros():
-    """
-    <sequenciaParametros> => <parametros> ',' <sequenciaParametros>
-                | <parametros>
-    """
+    '''
+    sequenciaParametros : parametros VIRGULA sequenciaParametros
+                | parametros
+    '''
     pass
 
 def p_lista_expressao():
-    """
-    <listaExpressao> => <sequenciaExpressao>
+    '''
+    listaExpressao : sequenciaExpressao
            | ε
-    """
+    '''
     pass
 
 def p_sequencia_literal():
-    """
-    <sequenciaLiteral> => <literal> <sequenciaLiteral>
-                  | <literal>
-    """
+    '''
+    sequenciaLiteral : literal sequenciaLiteral
+                  | literal
+    '''
     pass
 
 def p_lista_comandos():
-    """
-    <listaComandos> => <comando> <listaComandos>
+    '''
+    listaComandos : comando listaComandos
                 | ε
-    """
+    '''
     pass
 
 def p_sequencia_declaracao():
-    """
-    <sequenciaDeclaracao> => <declaracao> <sequenciaDeclaracao>
-              | <declaracao>
-    """
+    '''
+    sequenciaDeclaracao : declaracao sequenciaDeclaracao
+              | declaracao
+    '''
     pass
 
 def p_sequencia_spec_variavel():
-    """
-    <sequenciaSpecVariavel> => <specificacaoVariavel> ',' <sequenciaSpecVariavel>
-                  | <specificacaoVariavel>
-    """
+    '''
+    sequenciaSpecVariavel : specificacaoVariavel VIRGULA sequenciaSpecVariavel
+                  | specificacaoVariavel
+    '''
     pass
 
 def p_lista_declaracao_variavel():
-    """
-    <listaDeclaracaoVariavel> => <declaracaoVariavel> <listaDeclaracaoVariavel>
+    '''
+    listaDeclaracaoVariavel : declaracaoVariavel listaDeclaracaoVariavel
             | ε
-    """
+    '''
     pass
 
 def p_sequencia_expressao():
-    """
-    <sequenciaExpressao> => <expressao> ',' <sequenciaExpressao>
-              | <expressao>
-    """
+    '''
+    sequenciaExpressao : expressao VIRGULA sequenciaExpressao
+              | expressao
+    '''
     pass
-
-
-
-
-
-
-
-
-
